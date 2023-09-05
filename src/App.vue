705af2c8063bd2b393df55795bf60348fc60f833
Michael Simonett <mikesymo@michaels-imac.home>
2023-09-05 08:36:29 +0200

    setup and first component and global styles

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <h1>{{ title }}</h1>
+  <Modal />
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">Click me</button> -->
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  components: { Modal },
   data () {
     return {
       title: 'My First Vue App :)'
     }
+  },
+  methods: {
+    // handleClick() {
+    //   console.log(this.$refs.name)
+    //   this.$refs.name.classList.add('active')
+    //   this.$refs.name.focus()
+    // }
   }
 
 }
